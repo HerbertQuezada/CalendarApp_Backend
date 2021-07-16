e1d895490ed87e5f3882970e158985d1842009ea
Herbert <herber-1995@hotmail.com>
2021-07-16 02:00:26 -0400

    Se creo el AuthController "Para la autenticación" y el EventosController.

---
+++ CalendarApp_Backend/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarApp_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        // api/Auth/      Para Loguearse              -POST
+        /*
+            {
+                "name" : "Herbert",
+                "email" : "herber-1995@hotmacil.com",
+                "password" : "123456789"
+            }
+        */
+        // api/Auth/new   Para registrarse            -POST
+        // api/Auth/renew   Para actualizar el token  -GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
